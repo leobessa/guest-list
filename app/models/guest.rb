394f8cf5f249cff830a1c2d816c8a62c6cdb61d2
Leonardo Bessa <leobessa@gmail.com>
2010-05-10 03:29:06 -0300

    email :allow_blank => true

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -16,7 +16,7 @@ class Guest < ActiveRecord::Base
   end)
   belongs_to :invitation
   validates_presence_of :name, :category
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :allow_blank => true
 
   default_scope :order => 'id DESC'
 
